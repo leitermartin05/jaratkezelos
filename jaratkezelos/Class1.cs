2800ecbce65ceabf35fdf02693c3477c8a1b04c9
Leiter Martin <martosm05@gmail.com>
2021-02-05 13:05:53 +0100

    új járat felvétele és késés függvény fele

--- jaratkezelos/Class1.cs
+++ jaratkezelos/Class1.cs
@@ -8,14 +8,24 @@ namespace jaratkezelos
 {
     public class Class1
     {
+        List<Jarat> jaratok = new List<Jarat>();
         public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
         {
-
+            Jarat felveendo = new Jarat(jaratSzam, repterHonnan, repterHova, indulas);
         }
 
         public void Keses(string jaratSzam, int keses)
         {
+            foreach (var jarat in jaratok)
+            {
+                if (jarat.JaratSzam==jaratSzam)
+                {
+                    if (jarat.Keses+keses<0)
+                    {
 
+                    }
+                }
+            }
         }
 
         public DateTime MikorIndul(string jaratSzam)
---
+++ jaratkezelos/Jarat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaratkezelos
+{
+    class Jarat
+    {
+        private string jaratSzam;
+        private string honnanRepter;
+        private string hovaRepter;
+        private DateTime indulas;
+        private int keses;
+
+        public Jarat(string jaratSzam, string honnanRepter, string hovaRepter, DateTime indulas)
+        {
+            this.jaratSzam = jaratSzam;
+            this.honnanRepter = honnanRepter;
+            this.hovaRepter = hovaRepter;
+            this.indulas = indulas;
+            this.keses = 0;
+        }
+
+        public string JaratSzam { get => jaratSzam; set => jaratSzam = value; }
+        public string HonnanRepter { get => honnanRepter; set => honnanRepter = value; }
+        public string HovaRepter { get => hovaRepter; set => hovaRepter = value; }
+        public DateTime Indulas { get => indulas; set => indulas = value; }
+        public int Keses { get => keses; set => keses = value; }
+    }
+}
